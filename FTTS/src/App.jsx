31a18626cb0de749d6a961c811942df2c821aa80
ALVF <arindhimar116@gmail.com>
2025-01-18 19:36:46 +0530

    tried implementing text to speech

---
+++ FTTS/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TextToSpeech from "./components/TextToSpeech";
+
+function App() {
+  return (
+    <div>
+      <h1>Book Reader with TTS</h1>
+      <TextToSpeech />
+    </div>
+  );
+}
+
+export default App;
