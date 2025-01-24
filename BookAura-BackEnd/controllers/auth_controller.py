7f10a073db298f30e237975ef6806371193b03d8
ALVF <arindhimar116@gmail.com>
2025-01-24 17:35:33 +0530

    Fixed Files

--- BookAura-BackEnd/controllers/auth_controller.py
+++ BookAura-BackEnd/controllers/auth_controller.py
@@ -3,8 +3,7 @@ from models.users import UsersModel
 from models.platform_administrators import PlatformAdministratorsModel
 from models.normal_users import NormalUsersModel
 from models.moderator import ModeratorsModel
-
-
+from models.publisher import PublishersModel
 from models.roles import RolesModel
 
 import jwt
@@ -16,7 +15,7 @@ users_model = UsersModel()
 platform_administrators_model = PlatformAdministratorsModel()
 normal_users_model = NormalUsersModel()
 moderators_model = ModeratorsModel()
-
+publishers_model = PublishersModel()
 
 roles_model = RolesModel()
 
